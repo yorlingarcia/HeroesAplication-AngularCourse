c219bff2837f6791ed730ccefb46e2997dcf499d
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-25 13:09:09 -0500

    uso del sidenav y los iconos

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  exports: [MatButtonModule, MatSidenavModule, MatToolbarModule],
+  exports: [MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule],
 })
 export class MaterialModule {}
