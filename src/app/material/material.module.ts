b8d317ca1af3be417b47f9ddc54da87afdd99625
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-25 13:00:05 -0500

    se cambio el estilo de angular.json

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  exports: [MatSidenavModule],
+  exports: [MatButtonModule, MatSidenavModule, MatToolbarModule],
 })
 export class MaterialModule {}
