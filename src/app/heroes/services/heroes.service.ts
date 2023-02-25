3d981691034946ff32d5643d489a270feec4fa6c
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-25 18:47:12 -0500

    crecion del environments, para mejorar el uso del servicio

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,17 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Heroe } from '../interfaces/heroes.interfaces';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroesService {
+  private baseUrl: string = environment.baseUrl;
+
   constructor(private http: HttpClient) {}
   getHeores(): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>('/heroes');
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes`);
   }
 
   getHeroePorId(id: string): Observable<Heroe> {
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`);
   }
 }
