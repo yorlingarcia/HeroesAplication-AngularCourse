d92d756ebaa1b04fefcce033d6daa0437a8b6259
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-25 15:26:00 -0500

    importacion de la card de material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -9,6 +10,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   exports: [
     MatButtonModule,
     MatIconModule,
+    MatCardModule,
     MatListModule,
     MatSidenavModule,
     MatToolbarModule,
