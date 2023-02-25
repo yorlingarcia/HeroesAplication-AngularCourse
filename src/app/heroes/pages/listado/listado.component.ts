51e0670927e85069afb10109dda4fa078f44cf7d
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-25 15:46:49 -0500

    uso del fx Layout y ;a carta de angular

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -5,7 +5,13 @@ import { HeroesService } from '../../services/heroes.service';
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [],
+  styles: [
+    `
+      mat-card {
+        margin-top: 20px;
+      }
+    `,
+  ],
 })
 export class ListadoComponent implements OnInit {
   heroes: Heroe[] = [];
