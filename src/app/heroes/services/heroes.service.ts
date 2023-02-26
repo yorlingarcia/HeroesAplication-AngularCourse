a279ed3db984e8900b6d3cc39587883fa1198085
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-26 18:25:21 -0500

    creacion del servicio de  eliminacion de heroe, en el metodo delete no se necesita body, es decir no se le envia un heroe

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -32,4 +32,7 @@ export class HeroesService {
   actualizarHeroe(heroe: Heroe): Observable<Heroe> {
     return this.http.put<Heroe>(`${this.baseUrl}/heroes/${heroe.id}`, heroe);
   }
+  eliminarHeroe(heroe: Heroe): Observable<any> {
+    return this.http.delete<any>(`${this.baseUrl}/heroes/${heroe.id}`);
+  }
 }
