21377b7a6baeab1c83eb43bb787950087ceba242
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-25 19:23:47 -0500

    dentro de la opcioselected de mat autocomplite, se emitio el evento seleccionado, para obtener su evento

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { Heroe } from '../../interfaces/heroes.interfaces';
 import { HeroesService } from '../../services/heroes.service';
 
@@ -10,6 +11,7 @@ import { HeroesService } from '../../services/heroes.service';
 export class BuscarComponent {
   termino: string = '';
   heroes: Heroe[] = [];
+  heroeSelccionado!: Heroe;
 
   constructor(private heroesService: HeroesService) {}
 
@@ -17,5 +19,10 @@ export class BuscarComponent {
     this.heroesService
       .getSugerencias(this.termino)
       .subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  opcionSelecionada(event: MatAutocompleteSelectedEvent) {
+    const heroe: Heroe = event.option.value;
+    this.termino = heroe.superhero;
   }
 }
