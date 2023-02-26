bcbb83a41f761494faf75ca1567349e5118fc4cf
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-26 16:57:15 -0500

    creacion de los inputs con mat-form-field

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interfaces';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AgregarComponent {
+  heroe: Heroe = {
+    superhero: '',
+    publisher: Publisher.DCComics,
+    alter_ego: '',
+    first_appearance: '',
+    characters: '',
+  };
 
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'Dc - Comics',
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics',
+    },
+  ];
 }
