dfb7ec864bdd7aa0310ff14151a5056ea8c26b50
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-27 22:22:23 -0500

    creacion del nuevo servcio para validar el login

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,15 @@ import { Router } from '@angular/router';
   styles: [],
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authservices: AuthService) {}
 
   login() {
-    this.router.navigate(['/heroes']);
+    this.authservices.login().subscribe((resp) => {
+      console.log(resp);
+      if (resp.id) {
+        this.router.navigate(['/heroes']);
+      }
+      // this.router.navigate(['/heroes']);
+    });
   }
 }
