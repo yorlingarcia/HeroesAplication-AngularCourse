e6b6a4e64b26ed6022d77589a8fead852c4c9a45
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-28 14:54:40 -0500

    el acnload se usa para cargar el modulo y proteger la ruta, pero si esta precargado la ruta no es protegida

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanLoad {
+  constructor(private authService: AuthService) {}
+  // canActivate(
+  //   route: ActivatedRouteSnapshot,
+  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  //   return true;
+  // }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    // console.log('canLoad', false);
+    // console.log(route);
+    // console.log(segments);
+    if (this.authService.auth.id) {
+      return true;
+    }
+    console.log('Bloqueado por el authGuard');
+    return false;
+  }
+}
